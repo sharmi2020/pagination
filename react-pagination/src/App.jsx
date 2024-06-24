c20100956d1d25132fa18d0483a14d7348f1b7b3
Sharmila <107780645+sharmi2020@users.noreply.github.com>
2024-06-24 11:10:46 +0530

    first commit

---
+++ react-pagination/src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react';
+
+import Pagination from "./Components/Pagination";
+
+function App() {
+  
+  return (
+  <div>
+    <Pagination/>
+  </div>
+      )
+}
+export default App;
